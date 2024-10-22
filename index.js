19eaad8d123de8e4d7870edf4d69b55b361c9198
Walker2269-chichi <langatjeremiah22@gmail.com>
2024-10-22 17:19:16 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random=Math.floor(Math.random() * 10) +1;
+
+let num3 = 22;
+let num4 = 18;
+const mod = num3 % num4;
+
+const max = Math.max(4, 6, 8, 20);
